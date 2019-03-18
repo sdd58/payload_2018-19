718b00ae817fdda6f4d3bfd5ed32e36d203b1284
Sam DiPietro <sdd58@cornell.edu>
2019-03-18 09:04:58 -0400

    Commented out the accel

--- test.py
+++ test.py
@@ -7,10 +7,10 @@ import time
 imu = LSM9DS1(1, 0x6b, 0x1e)
 imu.begin()
 
-imu.initAccel()
+#imu.initAccel()
 imu.initGyro()
 
-imu.setAccelScale(2)
+#imu.setAccelScale(2)
 imu.setGyroScale(2)
 
 a_x = None
@@ -23,13 +23,13 @@ g_z = None
 
 while(1):
 
-    if (imu.accelAvailable()):
-        	imu.readAccel()
+    # if (imu.accelAvailable()):
+    #     	imu.readAccel()
 
-	        #Update the local variables
-	        a_x = imu.calcAccel(imu.ax)
-	    	a_y = imu.calcAccel(imu.ay)
-	    	a_z = imu.calcAccel(imu.az)
+	   #      #Update the local variables
+	   #      a_x = imu.calcAccel(imu.ax)
+	   #  	a_y = imu.calcAccel(imu.ay)
+	   #  	a_z = imu.calcAccel(imu.az)
 
     if (imu.gyroAvailable()):
 
@@ -40,6 +40,6 @@ while(1):
 	    	g_y = imu.calcGyro(imu.gy)
 	    	g_z = imu.calcGyro(imu.gz)
     
-    print("Acceleration: [",a_x,",",a_y,",",a_z,"]")
+    #print("Acceleration: [",a_x,",",a_y,",",a_z,"]")
     print("Gyroscope: [",g_x,",",g_y,",",g_z,"]")
 	time.sleep(1)
